07517f18a717095b9b826a73fa0ebd210626d714
stoldo <stoldo@runmyaccounts.com>
2021-02-05 09:46:20 +0100

    heroku stuff

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('./dist/m152-severintoldo'));
+
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/m152-severintoldo' }
+  );
+});
+
+app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`);
