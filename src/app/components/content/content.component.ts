fe0238897f26c5bf401f09b5dcfe99749e5802f0
stoldo <stoldo@runmyaccounts.com>
2021-03-05 10:22:35 +0100

    used content component

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
-  constructor() { }
+  @Input() public title: string;
 
-  ngOnInit(): void {
+
+  constructor() {
+  }
+
+  public ngOnInit(): void {
   }
 
 }
