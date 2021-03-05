bc23326a1f4752140d4a65bea5f48e28045d3318
stoldo <stoldo@runmyaccounts.com>
2021-03-05 09:47:02 +0100

    added some more copyright q&a, code clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,96 +1,17 @@
-import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
-import {BodyOutputType, Toast, ToasterConfig, ToasterService} from "angular2-toaster";
-import {Subscription} from "rxjs";
-import {
-  NgcCookieConsentService,
-  NgcInitializeEvent,
-  NgcNoCookieLawEvent,
-  NgcStatusChangeEvent
-} from "ngx-cookieconsent";
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit, OnInit, OnDestroy {
+export class AppComponent implements OnInit {
 
-  public toasterConfig : ToasterConfig = new ToasterConfig({
-    positionClass: 'toast-bottom-full-width',
-  });
-
-  public clicked = false;
-
-  // private popupOpenSubscription: Subscription;
-  // private popupCloseSubscription: Subscription;
-  // private initializeSubscription: Subscription;
-  // private statusChangeSubscription: Subscription;
-  // private revokeChoiceSubscription: Subscription;
-  // private noCookieLawSubscription: Subscription;
-
-
-  constructor(private toasterService: ToasterService) {
+  constructor() {
   }
 
-  public ngAfterViewInit(): void {
-    // const toast : Toast = {
-    //   type: 'warning',
-    //   // title: 'Title text',
-    //   body: 'This website uses cookies to ensure you get the best experience on our website. Learn more',
-    //   // body: CustomToastComponent,
-    //   // bodyOutputType: BodyOutputType.Component,
-    //   timeout: 0, // sticky
-    //   tapToDismiss: false,
-    //   onClickCallback: (toast: Toast) => {
-    //     console.log(toast);
-    //   },
-    //   showCloseButton: true,
-    //   closeHtml: '<button class="mt-3 mr-3">Got it</button>'
-    // };
-
-    // this.toasterService.pop(toast);
-
+  public ngOnInit(): void {
   }
 
-  ngOnInit(): void {
-    // // subscribe to cookieconsent observables to react to main events
-    // this.popupOpenSubscription = this.ccService.popupOpen$.subscribe(
-    //   () => {
-    //     // you can use this.ccService.getConfig() to do stuff...
-    //   });
-    //
-    // this.popupCloseSubscription = this.ccService.popupClose$.subscribe(
-    //   () => {
-    //     // you can use this.ccService.getConfig() to do stuff...
-    //   });
-    //
-    // this.initializeSubscription = this.ccService.initialize$.subscribe(
-    //   (event: NgcInitializeEvent) => {
-    //     // you can use this.ccService.getConfig() to do stuff...
-    //   });
-    //
-    // this.statusChangeSubscription = this.ccService.statusChange$.subscribe(
-    //   (event: NgcStatusChangeEvent) => {
-    //     // you can use this.ccService.getConfig() to do stuff...
-    //   });
-    //
-    // this.revokeChoiceSubscription = this.ccService.revokeChoice$.subscribe(
-    //   () => {
-    //     // you can use this.ccService.getConfig() to do stuff...
-    //   });
-    //
-    // this.noCookieLawSubscription = this.ccService.noCookieLaw$.subscribe(
-    //   (event: NgcNoCookieLawEvent) => {
-    //     // you can use this.ccService.getConfig() to do stuff...
-    //   });
-  }
-
-  ngOnDestroy(): void {
-    // this.popupOpenSubscription.unsubscribe();
-    // this.popupCloseSubscription.unsubscribe();
-    // this.initializeSubscription.unsubscribe();
-    // this.statusChangeSubscription.unsubscribe();
-    // this.revokeChoiceSubscription.unsubscribe();
-    // this.noCookieLawSubscription.unsubscribe();
-  }
 }
--- src/app/intercom/copyright/copyright.component.ts
+++ src/app/intercom/copyright/copyright.component.ts
@@ -16,27 +16,20 @@ export class CopyrightComponent implements OnInit {
     this.questionsAndAnswers.set('Wird ein Impressum vorgeschrieben?', 'Ja, in der Schweiz gilt eine Impressumspflicht (Siehe Gesetz gegen den unlauteren Wettbewerb (UWG))');
     this.questionsAndAnswers.set('Wie kann ich mein Geistiges Eigentum schützen?', 'Gewerblichen Schutzrechte wie Marken, Patente, Designs sowie das Urheberrecht');
     this.questionsAndAnswers.set('Was gibt es für Schutzkriterien?', 'Das Werk muss eine geistige Schöpfung sein und muss individuellen Charakter besitzen.');
-    this.questionsAndAnswers.set('Nennen sie 3 Beispiel für Urheberrechtsgeschütze Werke', 'a. literarische, wissenschaftliche und andere Sprachwerke\n' +
-      'b. Werke der Musik und andere akustische Werke\n' +
-      'c. Werke der bildenden Kunst, insbesondere der Malerei, der Bildhauerei und der Graphik\n' +
-      'd. Werke mit wissenschaftlichem oder technischem Inhalt wie Zeichnungen, Pläne, Karten oder plastische Darstellungen\n' +
-      'e. Werke der Baukunst\n' +
-      'f. Werke der angewandten Kunst\n' +
-      'g. fotografische, filmische und andere visuelle oder audiovisuelle Werke\n' +
-      'h. choreographische Werke und Pantomimen');
+    this.questionsAndAnswers.set('Drei Beispiel für Urheberrechtsgeschütze Werke:', '• literarische, wissenschaftliche und andere Sprachwerke<br/>• Werke der Musik und andere akustische Werke <br/>• Werke der bildenden Kunst, insbesondere der Malerei, der Bildhauerei und der Graphik <br/>• Werke mit wissenschaftlichem oder technischem Inhalt wie Zeichnungen, Pläne, Karten oder plastische Darstellungen <br/>• Werke der Baukunst <br/>• Werke der angewandten Kunst <br/>• fotografische, filmische und andere visuelle oder audiovisuelle Werke <br/>• choreographische Werke und Pantomimen');
     this.questionsAndAnswers.set('Können Teile, Entwürfe oder Titel von Werken geschützt werden?', 'Ja, wenn sie die Schutzkriterien erfüllen.');
     this.questionsAndAnswers.set('Wer ist Urheber?', 'Grundsätzlich gilt der bzw. die Erschaffer des Werkes (natürliche Person(en)) als Urheber, wenn nichts anderes bestimmt wurde.');
     this.questionsAndAnswers.set('Welche Rechte hat ein Urheber?', 'Der Urheber hat das Recht auf Erstveröffentlichung, sowie das Recht auf Anerkennung der Urheberschaft.');
     this.questionsAndAnswers.set('Wer kann Urheberrechtsverletzungen verfolgen?', 'Jeder einzelne Miturheber kann Urheberrechtsverletzungen verfolgen.');
-    this.questionsAndAnswers.set('Welche Nutzungsrechte hat ein Urheber?', 'a. Werkexemplare wie Druckerzeugnisse, Ton-, Tonbild- oder Datenträger herzustellen\n' +
-      'b. Werkexemplare anzubieten, zu veräussern oder sonstwie zu verbreiten\n' +
-      'c. das Werk direkt oder mit Hilfe irgendwelcher Mittel vorzutragen, aufzuführen, vorzuführen oder es anderswo wahrnehmbar zu machen\n' +
-      'd. das Werk durch Radio, Fernsehen oder ähnliche Einrichtungen, auch über Leitungen, zu senden\n' +
-      'e. gesendete Werke mit Hilfe von technischen Einrichtungen, deren Träger nicht das ursprüngliche Sendeunternehmen ist, insbesondere auch über Leitungen, weiterzusenden\n' +
-      'f. Sendungen und Weitersendungen wahrnehmbar zu machen');
+    this.questionsAndAnswers.set('Welche Nutzungsrechte hat ein Urheber?', '• Werkexemplare wie Druckerzeugnisse, Ton-, Tonbild- oder Datenträger herzustellen<br/>• Werkexemplare anzubieten, zu veräussern oder sonstwie zu verbreiten<br/>• das Werk direkt oder mit Hilfe irgendwelcher Mittel vorzutragen, aufzuführen, vorzuführen oder es anderswo wahrnehmbar zu machen<br/>• das Werk durch Radio, Fernsehen oder ähnliche Einrichtungen, auch über Leitungen, zu senden<br/>• gesendete Werke mit Hilfe von technischen Einrichtungen, deren Träger nicht das ursprüngliche Sendeunternehmen ist, insbesondere auch über Leitungen, weiterzusenden<br/>• Sendungen und Weitersendungen wahrnehmbar zu machen');
     this.questionsAndAnswers.set('Kann auf das Urheberrecht verzichtet werden?', 'Ja');
     this.questionsAndAnswers.set('Kann das Urheberrecht übertragen werden?', 'Ja, zu beachten ist das in der Schweiz das Urheberrecht auch unter Lebenden übertragen werden kann. Es ist somit übertragbar und vererblich.');
     this.questionsAndAnswers.set('Wie lange ist etwas urheberrechtlich geschützt?', 'Nach dem Tod des Urhebers 70 Jahre, ausser bei Computerprogrammen deren Urheberrechtsschutz läuft bereits nach 50 Jahren aus.');
+    this.questionsAndAnswers.set('Was sind und bedeuten Copyrights im Internet?', 'Copyright wird meist mit dem Urheberrecht gleichgesetzt. Das Urheberrecht schützt den Erfinder eines Werkes einschließlich seiner wirtschaftlichen und ideellen Rechte. Im Internet jedoch wird das Copyright oft verletzt. Zum Beispiel Die EU will dagegen mit dem umstrittenen Artikel 13 vorgehen.');
+    this.questionsAndAnswers.set('Was sind und bedeuten Lizenzen im Internet', 'Eine Lizenz ist eine Rechtskräftige Erlaubnis ein Werk nutzen zu dürfen. Einige Lizenzen erlauben auch die kommerzielle Nutzung. Bekannte Beispiele für Lizenzen sind die Creative Commons-, GNU GPL- oder MIT-Lizenz.');
+    this.questionsAndAnswers.set('Wer hat welche Copyrights auf meinem Internetauftritt?', 'Zurzeit sind alle Inhalte selbst produziert.');
+    this.questionsAndAnswers.set('Was ist "public domain"?', 'Wenn jemand auf sein Urheberrecht an einem Werk verzichtet wird dieses Teil der «public domain». Es kann dann frei und ohne Bedingungen genutzt werden.');
+    this.questionsAndAnswers.set('Was ist "fair use"?', '«Fair use» erlaubt die Nutzung eines urheberrechtsgeschützten Werkes solange sie der öffentlichen Bildung und der Anregung geistiger Produktionen dienen.');
   }
 }
 
