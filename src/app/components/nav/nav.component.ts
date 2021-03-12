286f676fec4092576e74781a4b82afc17216a6c8
stoldo <stoldo@runmyaccounts.com>
2021-03-12 09:53:12 +0100

    improved navbar

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,9 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked, AfterViewChecked,
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import {NavLink} from "../../model/nav-link.model";
 import {NavService} from "../../service/nav.service";
 import {homeRoute} from "../../shared/routes";
 import {Router} from "@angular/router";
-import {getCurrentRoute} from "../../shared/other.util";
+import {getCssProperties, getCssProperty, getCurrentRoute} from "../../shared/other.util";
+import {Subject} from "rxjs";
 
 
 @Component({
@@ -11,16 +21,25 @@ import {getCurrentRoute} from "../../shared/other.util";
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements OnInit, AfterViewChecked {
 
   public navLinks: NavLink[] = [];
   public isMenuCollapsed = true;
+  public isNavbarCollapsed = false;
 
   public readonly homeRoute = homeRoute();
 
+  @ViewChild('navbarToggler') navbarToggler: ElementRef;
+
 
   constructor(private navService: NavService,
-              private router: Router) {
+              private router: Router,
+              private changeDetectorRef: ChangeDetectorRef) {
+  }
+
+  @HostListener('window:resize')
+  public onResize(): void {
+    this.isNavbarCollapsed = this.isNavBarCollapsed();
   }
 
   public ngOnInit(): void {
@@ -38,5 +57,14 @@ export class NavComponent implements OnInit {
 
   public isNavLinkActive(navLink: NavLink): boolean {
     return navLink.route[0] === getCurrentRoute();
+  }
+
+  private isNavBarCollapsed(): boolean {
+    return getCssProperty(this.navbarToggler.nativeElement, 'display') !== 'none';
+  }
+
+  public ngAfterViewChecked(): void {
+    this.isNavbarCollapsed = this.isNavBarCollapsed();
+    this.changeDetectorRef.detectChanges();
   }
 }
--- src/app/shared/other.util.ts
+++ src/app/shared/other.util.ts
@@ -8,3 +8,18 @@ export function getCurrentRoute(): string {
 export function isBlankOrEmpty(s: string): boolean {
   return (s && s.trim().length === 0) || !s;
 }
+
+export function getCssProperties(nativeElement: any): Map<string, string> {
+  const css = window.getComputedStyle(nativeElement);
+  const cssProperties = new Map<string, string>();
+
+  for (let i = 0; i < css.length; i++) {
+    cssProperties.set(css[i], css.getPropertyValue(css[i]));
+  }
+
+  return cssProperties;
+}
+
+export function getCssProperty(nativeElement: any, name: string) {
+  return getCssProperties(nativeElement).get(name);
+}
