13b05841c80c1f71e6839e091e44d65dede51528
stoldo <48881931+severin-toldo@users.noreply.github.com>
2021-04-18 12:37:16 +0200

    finished logo and brand

--- src/app/intercom/style-guide/style-guide-logo-brand/style-guide-logo-brand.component.ts
+++ src/app/intercom/style-guide/style-guide-logo-brand/style-guide-logo-brand.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StyleGuideLogoBrandComponent implements OnInit {
 
-  constructor() { }
+  public readonly LOGO_BRAND_BODY = this.getLogoBrandBody();
 
-  ngOnInit(): void {
+
+  constructor() {
+  }
+
+  public ngOnInit(): void {
+  }
+
+  private getLogoBrandBody(): string {
+    return 'Das Logo der Severin-Toldo GmbH kann entweder eigenständig oder zusammen mit dem Schriftzug «Severin-Toldo GmbH» verwendet werden. Auch das Verwenden des eigenständigen Schriftzugs «Severin-Toldo GmbH» ist zulässig. Dabei ist wichtig, sowohl die untenstehenden Logo Vorgaben als auch die Typographischen Vorgaben im Kapitel «Typographie» zu befolgen.' +
+      '<br/><br/>' +
+      'In der Navigationsleiste darf nur der Schriftzug verwendet werden.' +
+      '<br/><br/>' +
+      'In Dokumenten darf nur die Logo-Schriftzug-Kombination verwendet werden.' +
+      '<br/><br/>' +
+      'Als Favoriten-Icon darf nur das Logo an sich verwendet werden.';
   }
 
 }
