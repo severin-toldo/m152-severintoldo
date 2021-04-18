407a33c83eb5098578fef06c69fcdf1e28eb6ba2
stoldo <48881931+severin-toldo@users.noreply.github.com>
2021-04-18 17:55:27 +0200

    added fav icon and tab text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Title} from "@angular/platform-browser";
 
 
 @Component({
@@ -8,10 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  constructor() {
+  constructor(private titleService: Title) {
   }
 
   public ngOnInit(): void {
+    this.titleService.setTitle('Severin-Toldo GmbH');
   }
 
 }
