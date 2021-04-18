64e13129e2882973da4b33a91da3e58b423c91f9
stoldo <48881931+severin-toldo@users.noreply.github.com>
2021-04-18 09:53:02 +0200

    dos and dont done

--- src/app/intercom/style-guide/style-guide-dos-donts/style-guide-dos-donts.component.ts
+++ src/app/intercom/style-guide/style-guide-dos-donts/style-guide-dos-donts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ComparisonCategory} from "../../../model/comparison-category.model";
+import {EMPTY_VALUE} from "../../../shared/other.util";
 
 @Component({
   selector: 'app-style-guide-dos-donts',
@@ -7,9 +9,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StyleGuideDosDontsComponent implements OnInit {
 
-  constructor() { }
+  public readonly DOS_COMPARISON_CATEGORIES = this.getDosComparisonCategories();
+  public readonly DONTS_COMPARISON_CATEGORIES = this.getDontsComparisonCategories();
 
-  ngOnInit(): void {
+
+  constructor() {
+  }
+
+  public ngOnInit(): void {
+  }
+
+  private getDosComparisonCategories(): ComparisonCategory[] {
+    const companyName = new ComparisonCategory();
+    companyName.title = 'Firmenname';
+    companyName.items = ['Severin-Toldo GmbH', EMPTY_VALUE, EMPTY_VALUE, EMPTY_VALUE, EMPTY_VALUE, EMPTY_VALUE];
+
+    const images = new ComparisonCategory();
+    images.title = 'Bilder';
+    images.items = ['Positive Bilder', 'Neutrale Bilder', 'Tier Bilder'];
+
+    const colorCombinations = new ComparisonCategory();
+    colorCombinations.title = 'Farbkombinationen';
+    colorCombinations.items = ['«Primary» / «White»', '«Secondary» / «White»'];
+
+    const words = new ComparisonCategory();
+    words.title = 'Tonalität und Wording';
+    words.items = ['preiswert', 'kompetent / professionell', 'Webseite', 'Webauftritt', 'Sie'];
+
+    return [companyName, images, colorCombinations, words];
+  }
+
+  private getDontsComparisonCategories(): ComparisonCategory[] {
+    const companyName = new ComparisonCategory();
+    companyName.title = 'Firmenname';
+    companyName.items = ['Severin Toldo GmbH', 'SeverinToldo GmbH', 'Severin-Toldo-GmbH', 'Severin-Toldo GMBH', 'Severin-Toldo GmBH', 'Severin-Toldo GmBh'];
+
+    const images = new ComparisonCategory();
+    images.title = 'Bilder';
+    images.items = ['Bilder von Unordnung, Chaos, Krieg usw.', EMPTY_VALUE, EMPTY_VALUE];
+
+    const colorCombinations = new ComparisonCategory();
+    colorCombinations.title = 'Farbkombinationen';
+    colorCombinations.items = ['«Primary» / «Success»', '«Success» / «Secondary»'];
+
+    const words = new ComparisonCategory();
+    words.title = 'Tonalität und Wording';
+    words.items = ['billig', 'gut', 'Website', 'Homepage', 'Du'];
+
+    return [companyName, images, colorCombinations, words];
   }
 
 }
