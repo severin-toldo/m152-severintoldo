456a6a416d9987c578d548f9cdefc9b6b2ae3f27
stoldo <48881931+severin-toldo@users.noreply.github.com>
2021-04-18 10:11:06 +0200

    wording content done

--- src/app/intercom/style-guide/style-guide-wording-content/style-guide-wording-content.component.ts
+++ src/app/intercom/style-guide/style-guide-wording-content/style-guide-wording-content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ComparisonCategory} from "../../../model/comparison-category.model";
+import {EMPTY_VALUE} from "../../../shared/other.util";
 
 @Component({
   selector: 'app-style-guide-wording-content',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StyleGuideWordingContentComponent implements OnInit {
 
-  constructor() { }
+  public readonly DOS_COMPARISON_CATEGORIES = this.getDosComparisonCategories();
+  public readonly DONTS_COMPARISON_CATEGORIES = this.getDontsComparisonCategories();
+  public readonly CONTENT_BODY = this.getContentBody();
 
-  ngOnInit(): void {
+
+  constructor() {
+  }
+
+  public ngOnInit(): void {
+  }
+
+  private getDosComparisonCategories(): ComparisonCategory[] {
+    const companyName = new ComparisonCategory();
+    companyName.title = 'Firmenname';
+    companyName.items = ['Severin-Toldo GmbH', EMPTY_VALUE, EMPTY_VALUE, EMPTY_VALUE, EMPTY_VALUE, EMPTY_VALUE];
+
+    const words = new ComparisonCategory();
+    words.title = 'Tonalität und Wording';
+    words.items = ['preiswert', 'kompetent / professionell', 'Webseite', 'Webauftritt', 'Sie'];
+
+    return [companyName, words];
+  }
+
+  private getDontsComparisonCategories(): ComparisonCategory[] {
+    const companyName = new ComparisonCategory();
+    companyName.title = 'Firmenname';
+    companyName.items = ['Severin Toldo GmbH', 'SeverinToldo GmbH', 'Severin-Toldo-GmbH', 'Severin-Toldo GMBH', 'Severin-Toldo GmBH', 'Severin-Toldo GmBh'];
+
+    const words = new ComparisonCategory();
+    words.title = 'Tonalität und Wording';
+    words.items = ['billig', 'gut', 'Website', 'Homepage', 'Du'];
+
+    return [companyName, words];
+  }
+
+  private getContentBody(): string {
+    return 'Die Webseite soll mit häufig gebrauchten Web-Komponenten populiert werden, welche potenzielle Kunden direkt ausprobieren können. Es sollen dafür sowohl grafische, textuelle als auch multimediale Elemente verwendet werden. Dabei soll darauf geachtet werden, stets ein professionelles und modernes Ambiente zu wahren (= Gestaltungsprinzipien).';
   }
 
 }
