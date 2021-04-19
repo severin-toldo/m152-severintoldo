c411498f7cce1352cd8f3446ff3530a41a5180e7
stoldo <48881931+severin-toldo@users.noreply.github.com>
2021-04-19 22:30:05 +0200

    populated home component

--- src/app/intercom/home/home.component.ts
+++ src/app/intercom/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { contactRoute } from 'src/app/shared/routes';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public readonly contactRoute = contactRoute;
 
-  ngOnInit(): void {
+
+  constructor() {
+  }
+
+  public ngOnInit(): void {
   }
 
 }
