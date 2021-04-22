b69bea5a35eec03e27c66aedb3ecb2b104a1055d
stoldo <48881931+severin-toldo@users.noreply.github.com>
2021-04-22 23:48:32 +0200

    added compression to express server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const compression = require('compression')
 const express = require('express');
+
 const app = express();
 
+app.use(compression())
 app.use(express.static('./dist/m152-severintoldo'));
 
 app.get('/*', function (req, res) {
